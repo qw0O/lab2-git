b07a9ea47aafac1e5810e5e521c06a91d948f962
qw0O <yula.mat.2007@gmail.com>
2026-05-19 18:56:10 +0300

    Change greeting to Developer One version

--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ int calculateMagic(int a, int b) {
 int main() {
     std::cout << "Magic Result: " << calculateMagic(10, 25) << std::endl;
 
-    std::cout << "Hello" << std::endl;
+    std::cout << "Hello from Developer One" << std::endl;
     return 0;
 }
